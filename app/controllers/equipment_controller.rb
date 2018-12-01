09a68f689336f773603ad79b6ce7552c95099ad3
Alan Vardy <alanvardy@gmail.com>
2018-12-01 16:15:31 -0700

    wip

--- app/controllers/equipment_controller.rb
+++ app/controllers/equipment_controller.rb
@@ -1,6 +1,10 @@
 class EquipmentController < ApplicationController
   def all
-    # expires_in 1.minute, public: true
-    @equipment = Equipment.all
+    expires_in 1.minute, public: true
+    @equipment = Equipment.all.reverse
+  end
+
+  def show
+    @equipment = Equipment.find(params[:id])
   end
 end
