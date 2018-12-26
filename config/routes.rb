8ca646ebdeed8a4ed44a2cf76f3d911ad1e62cff
Alan Vardy <alanvardy@gmail.com>
2018-12-26 09:01:38 -0700

    add CHECKS

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,7 @@ Rails.application.routes.draw do
   root 'sites#index'
   get 'equipment/index'
   get 'equipment/:id', to:'equipment#show'
+
+  get '/check.txt', to: proc {[200, {}, ['it_works']]}
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
