968bad042ca95e7b5790a65eed39a06fac0778b4
Alan Vardy <alanvardy@gmail.com>
2019-01-02 10:10:42 -0700

    improved styling and buttons

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_02_004201) do
+ActiveRecord::Schema.define(version: 2019_01_02_163719) do
 
   create_table "connections", force: :cascade do |t|
     t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "site_id"
+    t.string "title"
     t.index ["user_id"], name: "index_connections_on_user_id"
   end
 
