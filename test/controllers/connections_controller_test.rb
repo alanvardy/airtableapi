36812a4abc76f9c9760165d0de82637c1ebd05fb
Alan Vardy <alanvardy@gmail.com>
2019-01-04 20:10:02 -0700

    add connections tests

--- test/controllers/connections_controller_test.rb
+++ test/controllers/connections_controller_test.rb
@@ -4,15 +4,38 @@ require 'test_helper'
 
 class ConnectionsControllerTest < ActionDispatch::IntegrationTest
   setup do
-    # @permission = Permission.create(title: 'Client', value: 0)
-    # @client = User.create(name: 'client', email: 'client@client.com', password: 'password', password_confirmation: 'password', permission_id: 1)
-    # @connection = Connection.create(user_id: 1, site_id: 'xxx')
+    @p0 = Permission.create(title: 'Client', value: 0)
+    @p1 = Permission.create(title: 'Technician', value: 1)
+    @p2 = Permission.create(title: 'Manager', value: 2)
+    @client = User.create(name: 'Client', email: 'client@a.com', password: 'password', password_confirmation: 'password', permission_id: @p0.id)
+    @technician = User.create(name: 'Technician', email: 'technician@a.com', password: 'password', password_confirmation: 'password', permission_id: @p1.id)
+    @manager = User.create(name: 'Manager', email: 'manager@a.com', password: 'password', password_confirmation: 'password', permission_id: @p2.id)
+
+    @site = Site.all.first
+    @connected_site = Site.all.last
+
+    @connection = Connection.create(user_id: @client.id, site_id: @connected_site.id)
   end
 
-  # test "shouldn't get index when not logged in" do
-  #   get connections_url
-  #   assert_redirected_to login_path
-  # end
+  test "shouldn't get index when not logged in" do
+    get connections_url
+    assert_redirected_to login_path
+  end
+
+  test "shouldn't get index when insufficient permissions" do
+    log_in @client
+    get connections_url
+    assert_redirected_to user_path(@client)
+    log_in @technician
+    get connections_url
+    assert_redirected_to user_path(@technician)
+  end
+
+  test 'should get index when sufficient permissions' do
+    log_in @manager
+    get connections_url
+    assert_response :success
+  end
 
   test "shouldn't get new when not logged in" do
     get new_connection_url
@@ -20,34 +43,89 @@ class ConnectionsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "shouldn't get new when insufficient access permissions" do
-    @client = create_client
+    log_in @client
     get new_connection_url
     assert_redirected_to user_path(@client)
+    log_in @technician
+    get new_connection_url
+    assert_redirected_to user_path(@technician)
   end
 
   test 'should get new when sufficient access permissions' do
-    create_manager
+    log_in @manager
     get new_connection_url
     assert_response :success
   end
 
-  # test "should create connection" do
-  #   assert_difference('Connection.count') do
-  #     post connections_url, params: { connection: { sites_id: @connection.sites_id, users_id: @connection.users_id } }
-  #   end
+  test "shouldn't create connection when not logged in" do
+    assert_no_difference('Connection.count') do
+      post connections_url, params: { connection: { site_id: @connection.site_id, user_id: @connection.user_id } }
+    end
 
-  #   assert_redirected_to connection_url(Connection.last)
-  # end
+    assert_redirected_to login_path
+  end
 
-  # test "should show connection" do
-  #   get connection_url(@connection)
-  #   assert_response :success
-  # end
+  test "shouldn't create connection when insufficient access permissions" do
+    log_in @client
+    assert_no_difference('Connection.count') do
+      post connections_url, params: { connection: { site_id: @connection.site_id, user_id: @connection.user_id } }
+    end
+    assert_redirected_to user_path(@client)
+    log_in @technician
+    assert_no_difference('Connection.count') do
+      post connections_url, params: { connection: { site_id: @connection.site_id, user_id: @connection.user_id } }
+    end
+    assert_redirected_to user_path(@technician)
+  end
 
-  # test "should get edit" do
-  #   get edit_connection_url(@connection)
-  #   assert_response :success
-  # end
+  test 'should create connection when sufficient access permissions' do
+    log_in @manager
+    assert_difference('Connection.count') do
+      post connections_url, params: { connection: { site_id: @site.id, user_id: @client.id } }
+    end
+
+    assert_redirected_to connection_url(Connection.last)
+  end
+
+  test "shouldn't show connection when not logged in" do
+    get connection_url(@connection)
+    assert_redirected_to login_path
+  end
+
+  test "shouldn't show connection when insufficient access permissions" do
+    log_in @client
+    get connection_url(@connection)
+    assert_redirected_to user_path(@client)
+    log_in @technician
+    get connection_url(@connection)
+    assert_redirected_to user_path(@technician)
+  end
+
+  test 'should show connection when when sufficient access permissions' do
+    log_in @manager
+    get connection_url(@connection)
+    assert_response :success
+  end
+
+  test "shouldn't get edit when not logged in" do
+    get edit_connection_url(@connection)
+    assert_redirected_to login_path
+  end
+
+  test "shouldn't get edit when not insufficient access permissions" do
+    log_in @client
+    get edit_connection_url(@connection)
+    assert_redirected_to user_path(@client)
+    log_in @technician
+    get edit_connection_url(@connection)
+    assert_redirected_to user_path(@technician)
+  end
+
+  test 'should get edit when sufficient access permissions' do
+    log_in @manager
+    get edit_connection_url(@connection)
+    assert_response :success
+  end
 
   # test "should update connection" do
   #   patch connection_url(@connection), params: { connection: { sites_id: @connection.sites_id, users_id: @connection.users_id } }
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -8,20 +8,4 @@ class ActiveSupport::TestCase
   def log_in(user)
     post login_path, params: { email: user.email, password: 'password' }
   end
-
-  def create_client
-    @permission = Permission.create(title: 'Client', value: 0)
-    @client = User.create(name: 'client', email: 'client@client.com', password: 'password', password_confirmation: 'password', permission_id: Permission.first.id)
-    @connection = Connection.create(user_id: 1, site_id: 'xxx')
-    log_in(@client)
-    @client
-  end
-
-  def create_manager
-    @permission = Permission.create(title: 'Manager', value: 2)
-    @manager = User.create(name: 'manager', email: 'manager@manager.com', password: 'password', password_confirmation: 'password', permission_id: Permission.first.id)
-    @connection = Connection.create(user_id: 1, site_id: 'xxx')
-    log_in(@manager)
-    @manager
-  end
 end
